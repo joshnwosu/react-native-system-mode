96dc45512084db8eaea4a7c8e0f11728fb771193
joshnwosu <joshuanwosu078@gmail.com>
2022-06-27 10:10:55 +0100

    updated details screen

---
+++ src/screens/DetailsScreen.js
@@ -0,0 +1,15 @@
+import { Button, Text, View } from "react-native";
+
+export default DetailsScreen = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push("Details")}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+};
