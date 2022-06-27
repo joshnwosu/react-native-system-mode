2ccf41bab7267dc2e692b161b3ee6730ad91e25e
joshnwosu <joshuanwosu078@gmail.com>
2022-06-27 10:12:13 +0100

    update expore screen

---
+++ src/screens/ExploreScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function ExploreScreen() {
+  return (
+    <View>
+      <Text>Explore</Text>
+    </View>
+  );
+}
