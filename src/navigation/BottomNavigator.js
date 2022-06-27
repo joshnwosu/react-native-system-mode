78fb9c0a63a2ea88b0895fabc3ccea7f7c2561dc
joshnwosu <joshuanwosu078@gmail.com>
2022-06-27 10:12:50 +0100

    update

---
+++ src/navigation/BottomNavigator.js
@@ -0,0 +1,100 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ExploreScreen from "../screens/ExploreScreen";
+import HomeScreen from "../screens/HomeScreen";
+import NotificationScreen from "../screens/NotificationScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+import { Ionicons, Octicons } from "@expo/vector-icons";
+import { StyleSheet, View } from "react-native";
+import { BlurView } from "expo-blur";
+import HomeNavigator from "./HomeNavigator";
+import NotificationNavigator from "./NotificationNavigator";
+
+const Tab = createBottomTabNavigator();
+
+const BottomNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        // trans
+        // tabBarShowLabel: false,
+        tabBarLabelPosition: "below-icon",
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "bold",
+        },
+        tabBarStyle: { position: "absolute" },
+        tabBarBackground: () => (
+          <BlurView
+            tint="light"
+            intensity={100}
+            style={StyleSheet.absoluteFill}
+          />
+        ),
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeNavigator}
+        options={{
+          tabBarIcon: ({ color, size, focused }) => (
+            <View>
+              <Ionicons
+                name={focused ? "home" : "home-outline"}
+                color={color}
+                size={20}
+              />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={NotificationNavigator}
+        options={{
+          tabBarIcon: ({ color, size, focused }) => (
+            <View>
+              <Octicons
+                name={focused ? "bell-fill" : "bell"}
+                color={color}
+                size={20}
+              />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Explore"
+        component={ExploreScreen}
+        options={{
+          tabBarIcon: ({ color, size, focused }) => (
+            <View>
+              <Octicons
+                name={focused ? "telescope-fill" : "telescope"}
+                color={color}
+                size={20}
+              />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ color, size, focused }) => (
+            <View>
+              <Octicons
+                name={focused ? "person-fill" : "person"}
+                color={color}
+                size={20}
+              />
+            </View>
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomNavigator;
