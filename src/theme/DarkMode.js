070bb1f99e0c46d950c335c4295708a9059351c0
joshnwosu <joshuanwosu078@gmail.com>
2022-06-28 11:56:45 +0100

    create theme

---
+++ src/theme/DarkMode.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const darkMode = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    backgroundColor: "#212529",
+  },
+});
+
+export default darkMode;
