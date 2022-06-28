d53714a0a73523145811ae606d1a513d04cb22ba
joshnwosu <joshuanwosu078@gmail.com>
2022-06-28 11:56:10 +0100

    update

--- src/navigation/ExploreNavigator.js
+++ src/navigation/ExploreNavigator.js
@@ -1,28 +1,40 @@
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Text, TouchableOpacity } from "react-native";
+import { Platform, Text, TouchableOpacity } from "react-native";
 import NotificationScreen from "../screens/NotificationScreen";
+import ExploreScreen from "../screens/ExploreScreen";
 const Stack = createNativeStackNavigator();
 
 export default function ExploreNavigator() {
   return (
     <Stack.Navigator
       screenOptions={{
-        headerTransparent: true,
+        headerTransparent: Platform.OS == "ios" ? true : false,
         headerBlurEffect: "prominent",
+        headerLargeStyle: {
+          backgroundColor: "#F8F7F8",
+        },
+        headerStyle: {
+          backgroundColor: Platform.OS == "android" && "#F8F7F8",
+        },
+        headerShadowVisible: false,
       }}
     >
       <Stack.Screen
         name="ExploreScreen"
-        component={NotificationScreen}
-        options={{
+        component={ExploreScreen}
+        options={({}) => ({
           title: "Explore",
           headerLargeTitle: true,
-          headerSearchBarOptions: {
-            placeholder: "Github search",
-          },
-        }}
+          headerSearchBarOptions:
+            Platform.OS == "ios"
+              ? {
+                  placeholder: "Github search",
+                  headerIconColor: "red",
+                }
+              : null,
+        })}
       />
     </Stack.Navigator>
   );
