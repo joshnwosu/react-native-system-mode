8f4e5103efd5842e35c6801331be35fb1a74e03b
joshnwosu <joshuanwosu078@gmail.com>
2022-06-29 09:46:28 +0100

    update

--- src/screens/ExploreScreen.js
+++ src/screens/ExploreScreen.js
@@ -1,10 +1,18 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import themeContext from "../config/themeContext";
 
 export default function ExploreScreen() {
+  const theme = useContext(themeContext);
   return (
-    <View>
+    <View style={[styles.container]}>
       <Text></Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
