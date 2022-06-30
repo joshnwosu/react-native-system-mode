5f5d146dab72551f1fe7db54c0219008d6f38efd
joshnwosu <joshuanwosu078@gmail.com>
2022-06-30 19:19:22 +0100

    update

--- src/config/theme.js
+++ src/config/theme.js
@@ -1,15 +1,70 @@
+const name = "mint";
+
 const theme = {
   light: {
     theme: "light",
-    color: "#000000",
+    color: "#222222",
     background: "#ffffff",
     card: "#F8F7F8",
+    label: "#999",
+    default: {
+      color: "#222222",
+      card: "#FFFFFF",
+      label: "#777777",
+      header: "#F8F7F8",
+      background: "#F8F7F8",
+    },
+    ice: {
+      color: "#222222",
+      header: "#529AF910",
+      background: "#529AF910",
+      card: "#FFFFFF",
+      label: "#529AF9",
+    },
+    fire: {
+      color: "#222222",
+      header: "#fffaeb",
+      background: "#fffaeb",
+      card: "#FFFFFF",
+      label: "#f79009",
+    },
+    trees: {
+      color: "#222222",
+      header: "#ecfdf3",
+      background: "#ecfdf3",
+      card: "#FFFFFF",
+      label: "#12bf68",
+    },
+    pony: {
+      color: "#030749",
+      header: "#fef3f2",
+      background: "#fef3f2",
+      card: "#FFFFFF",
+      label: "#f04438",
+    },
   },
+
   dark: {
     theme: "dark",
-    color: "#ffffff",
+    color: "#DDDDDD",
     background: "#000000",
-    card: "#090909",
+    header: "#000000",
+    card: "#000000",
+    label: "#AAAAAA",
+    night: {
+      color: "#DDDDDD",
+      background: "#000000",
+      header: "#000000",
+      card: "#111111",
+      label: "#AAAAAA",
+    },
+    amoled: {
+      color: "#DDDDDD",
+      background: "#000000",
+      header: "#000000",
+      card: "#000000",
+      label: "#999999",
+    },
   },
 };
 
